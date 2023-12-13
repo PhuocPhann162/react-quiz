8ae3b718cdbada8604e9cf6bcf5aff76499889a3
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-12-13 21:57:23 +0700

    fetch fake data api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "1":
+      return { ...state, status: "active" };
+    case "2":
+      return { ...state, status: "finished" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:9000/questions");
+        const data = await res.json();
+        console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
